25a3cbe55cd26ad2dbeda64548d948dcc91d416a
Sergio Gonzalez <sergonza@iu.edu>
2026-04-07 02:36:32 -0400

    [Finished] Image hotspots

--- src/main/java/com/swe/project/contentaccess/controller/TopicController.java
+++ src/main/java/com/swe/project/contentaccess/controller/TopicController.java
@@ -1,27 +1,35 @@
 package com.swe.project.contentaccess.controller;
 
+import com.swe.project.contentaccess.model.Topic;
+import com.swe.project.contentaccess.service.TopicService;
 import org.springframework.web.bind.annotation.*;
-import java.util.Map;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/topics")
+@CrossOrigin(origins = "*")
 public class TopicController {
 
-    public TopicController() {
+    private final TopicService topicService;
+
+    public TopicController(TopicService topicService) {
+        this.topicService = topicService;
         System.out.println(">>> TopicController CREATED <<<");
     }
-    
+
     @GetMapping("/test")
     public String test() {
         return "OK";
     }
 
+    @GetMapping
+    public List<Topic> getAllTopics() {
+        return topicService.getAllTopics();
+    }
+
     @GetMapping("/{id}")
-    public Map<String, String> getTopic(@PathVariable String id) {
-        return Map.of(
-                "id", id,
-                "name", "Test Topic",
-                "status", "content-access working"
-        );
+    public Topic getTopic(@PathVariable String id) {
+        return topicService.getTopicById(id);
     }
 }
--- src/main/java/com/swe/project/contentaccess/service/TopicService.java
+++ src/main/java/com/swe/project/contentaccess/service/TopicService.java
@@ -1,10 +1,98 @@
 package com.swe.project.contentaccess.service;
 
+import com.swe.project.contentaccess.model.Hotspot;
 import com.swe.project.contentaccess.model.Topic;
-import com.swe.project.contentaccess.repository.TopicRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class TopicService {
 
+    private final List<Topic> topics = List.of(
+            new Topic(
+                    "fruits",
+                    "Fruits",
+                    "/images/fruits.jpg",
+                    "Food",
+                    List.of(
+                            new Hotspot(
+                                    "Apple",
+                                    "/images/apple.jpg",
+                                    10.0,
+                                    53.0,
+                                    Map.of("Spanish", "Manzana"),
+                                    Map.of()
+                            ),
+                            new Hotspot(
+                                    "Orange",
+                                    "/images/orange.jpg",
+                                    24.0,
+                                    23.0,
+                                    Map.of("Spanish", "Naranja"),
+                                    Map.of()
+                            ),
+                            new Hotspot(
+                                    "Kiwi",
+                                    "/images/kiwi.jpg",
+                                    67.0,
+                                    35.0,
+                                    Map.of("Spanish", "Kiwi"),
+                                    Map.of()
+                            ),
+                            new Hotspot(
+                                    "Raspberry",
+                                    "/images/raspberry.jpg",
+                                    81.0,
+                                    67.0,
+                                    Map.of("Spanish", "Frambuesa"),
+                                    Map.of()
+                            )
+                    )
+            ),
+            new Topic(
+                    "vegetables",
+                    "Vegetables",
+                    "/images/vegetables.jpg",
+                    "Food",
+                    List.of(
+                            new Hotspot(
+                                    "Carrot",
+                                    "/images/carrot.jpg",
+                                    52.0,
+                                    37.0,
+                                    Map.of("Spanish", "Zanahoria"),
+                                    Map.of()
+                            ),
+                            new Hotspot(
+                                    "Garlic",
+                                    "/images/garlic.jpg",
+                                    14.0,
+                                    75.0,
+                                    Map.of("Spanish", "Ajo"),
+                                    Map.of()
+                            ),
+                            new Hotspot(
+                                    "Potato",
+                                    "/images/potato.jpg",
+                                    82.0,
+                                    52.0,
+                                    Map.of("Spanish", "Papa"),
+                                    Map.of()
+                            )
+                    )
+            )
+    );
+
+    public List<Topic> getAllTopics() {
+        return topics;
+    }
+
+    public Topic getTopicById(String id) {
+        return topics.stream()
+                .filter(topic -> topic.getId().equalsIgnoreCase(id))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Topic not found: " + id));
+    }
 }
